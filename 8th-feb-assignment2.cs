33990b41e9b8453b0e38a88d2dfe225380d03bd6
sumiya894387 <77432659+sumiya894387@users.noreply.github.com>
2021-02-09 12:30:07 +0530

    Add files via upload

--- 8th-feb-assignment2.cs
+++ 8th-feb-assignment2.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace modularity1
+namespace assignment3
 {
-    class Program
+    class Class1
     {
         static void Main(string[] args)
         {
-          //1.    print sum and average of elements of array
+            //1.    print sum and average of elements of array
             int[] arr = new int[10];
             float sum = 0;
             float avg;
             int i, j;
-            int[] sum2 = new int[2];
-            int[] sum3 = new int[2];
+            int[] sum2 = { 0, 0, 0, 0 };
+            int[] sum3 = { 0, 0, 0, 0 };
             int[,] num1 = new int[2, 2];
             Console.WriteLine("enter numbers");
             for (i = 0; i < 10; i++)
@@ -26,7 +26,7 @@ namespace modularity1
             }
             avg = sum / 10;
             Console.WriteLine("sum is" + " " + sum + " " + "and avg is" + " " + avg);
-           //2.program to display sum of 3 by 3 matrices
+            //2.program to display sum of 3 by 3 matrices
 
             int sum1 = 0;
             int[,] arr1 = new int[3, 3];
@@ -47,7 +47,7 @@ namespace modularity1
             }
             Console.WriteLine("sum is" + " " + sum);
 
-             //3.program to find maximum element of an integer array
+            //3.program to find maximum element of an integer array
 
             Console.WriteLine("enter numbers");
             for (i = 0; i < 10; i++)
@@ -58,7 +58,7 @@ namespace modularity1
             int max = arr[0];
             for (i = 0; i < 10; i++)
             {
-                
+
                 if (max < arr[i])
                 {
                     max = arr[i];
@@ -66,7 +66,7 @@ namespace modularity1
             }
             Console.WriteLine("maximum element is" + " " + max);
             //4.program to print column wise sum of elements of 2d array
-         
+
             Console.WriteLine("enter numbers to print column wise sum of elements");
             for (i = 0; i < 2; i++)
             {
@@ -85,23 +85,22 @@ namespace modularity1
                 Console.WriteLine();
             }
             Console.WriteLine("sum of columns");
-            
-            for (i = 0; i < 3; i++)
+
+            for (i = 0; i < 2; i++)
             {
-                sum2[i] = 0;
-                for (j = 0; j < 3; j++)
+                for (j = 0; j < 2; j++)
                 {
 
                     sum2[i] += num1[j, i];
                 }
-                
+
             }
             for (j = 0; j < 2; j++)
             {
                 Console.WriteLine(sum2[j]);
             }
             //5.PROGRAM TO PRINT ROW WISE SUM OF ELEMENTS
-           
+
             Console.WriteLine("enter numbers to print ROW wise sum of elements");
             for (i = 0; i < 2; i++)
             {
@@ -122,27 +121,29 @@ namespace modularity1
             Console.WriteLine("sum of ROWS");
             for (i = 0; i < 2; i++)
             {
-                sum3[i] = 0;
+
 
                 for (j = 0; j < 2; j++)
                 {
 
                     sum3[i] += num1[i, j];
                 }
-                
+
             }
-           
-            
+
+
             for (i = 0; i < 2; i++)
             {
 
-            }
-            for (j = 0; j < 2; j++)
-            {
-                Console.WriteLine(sum3[i]);
-            }
+          
+                    Console.WriteLine(sum3[i]);
+                }
+            
         }
+    
+
+
+
+
     }
-
 }
-
